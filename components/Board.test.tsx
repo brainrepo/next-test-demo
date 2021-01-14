bdcc0fdf75fb188c5a79d821b2d9441cbffb62ed
brainrepo <murru7@gmail.com>
2021-01-14 19:27:14 +0100

    Regexp tools

--- components/Board.test.tsx
+++ components/Board.test.tsx
@@ -9,10 +9,17 @@ describe('<Board />', () => {
     })
     it('it have to show hits', () => {
         const { getByText } = render(<Board hits={12} points={22} />);
-        getByText(/Hits: 12/)
+        getByText(/Hits: 12/) //It support full regex
     })
-    it('it have to show points', () => {
-        const { getByText } = render(<Board hits={12} points={22} />);
-        getByText(/Points: 22/)
+    it('it have to show points using regex', () => {
+        const hits:Number=12, points:Number=22;
+        const { getByText } = render(<Board hits={hits} points={points} />);
+        getByText(new RegExp(`Points: ${points}`, 'i'))
+        getByText(new RegExp(`Hits: ${hits}`, 'i'))
+    })
+    it('it this component is dumb', () => {
+        const { getByText } = render(<Board hits={-2} points={1000} />);
+        getByText(/Points: 1000/)
+        getByText(/Hits: -2/)
     })
 })
